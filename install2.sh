cb1942f283d1bc59a4f857b6b44a8a1b35bff7c9
Philip Hill <philip.hill@hidglobal.com>
2025-09-17 14:03:32 +0100

    (wip) Attempt to support tidl tools, however stuck on issues downloading missing files from TI website - ticket has been raised.

--- install2.sh
+++ install2.sh
@@ -6,10 +6,10 @@ export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init - bash)"
 
-pyenv install 3.10.18
-pyenv virtualenv 3.10.18 py310
+pyenv install 3.10.0
+pyenv virtualenv 3.10.0 py3100
 pyenv rehash
-pyenv activate py310
-pip3 install --no-input --upgrade pip==25.2 setuptools==73.0.0
-
-python -m pip install --upgrade pip
+pyenv activate py3100
+pip3 install --no-input --upgrade pip==24.2 setuptools==73.0.0
+pip3 install --no-input cython wheel numpy==1.23.0
+#python -m pip install --upgrade pip
---
+++ install_tidl.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -xe
+
+sudo apt-get install -y libyaml-cpp-dev libglib2.0-dev wget python3 python3-pip cmake
+sudo mkdir -p /opt/ti
+sudo chmod 777 /opt/ti
+cd /opt/ti
+git clone https://github.com/TexasInstruments/edgeai-tidl-tools.git
+export SOC=am62a
+cd edgeai-tidl-tools
+source ./setup.sh
+
+
+# fails on 
+# wget  https://software-dl.ti.com/jacinto7/esd/tidl-tools/11_00_06_00/TIDL_TOOLS/AM62A/tidl_tools.tar.gz
+# Ticket raised on TI 
+# https://e2e.ti.com/support/processors-group/processors/f/processors-forum/1566728/am62a7-edgeai-tidl-tools---setup-instructions-fail-file-tidl_tools-tar-gz-unavailable-for-download?tisearch=e2e-sitesearch&keymatch=tidl_tools.tar.gz#
