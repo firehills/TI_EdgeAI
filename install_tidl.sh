f5d31035db609402b76b86f4b878554206efebbc
Philip Hill <philip.hill@hidglobal.com>
2025-09-18 13:40:28 +0100

    (fix) tidl - adds required dependencies. the setup.sh should be run outside of the script as it sets env vars.

--- install_tidl.sh
+++ install_tidl.sh
@@ -2,26 +2,35 @@
 
 set -xe
 
-sudo apt-get install -y libyaml-cpp-dev libglib2.0-dev wget python3 python3-pip cmake
+sudo apt-get install -y libyaml-cpp-dev libglib2.0-dev wget python3 python3-pip cmake graphviz ffmpeg libsm6 libxext6
 sudo mkdir -p /opt/ti
 sudo chmod 777 /opt/ti
 cd /opt/ti
 git clone https://github.com/TexasInstruments/edgeai-tidl-tools.git
+cd /opt/ti/edgeai-tidl-tools
+# Fix to a specifc revision
+git checkout 95ba2c7ec62bbedeb637d7a5c0273fcede21cac9
 export SOC=am62a
 
 
 mkdir -p /opt/ti/edgeai-tidl-tools/tools/AM62A/
 cd /opt/ti/edgeai-tidl-tools/tools/AM62A/
 cp /workspaces/TI_EdgeAI/tidl_tools.tar.gz .
-tar xzvf tidl_tools.tar.gz
+#tar xzvf tidl_tools.tar.gz
 tar xzvf tidl_tools.tar.gz  --strip-components=1
 
-sed -i 's!wget --quiet   https://software-dl.ti.com/jacinto7/esd/tidl-tools/$REL/TIDL_TOOLS/${SOC^^}/tidl_tools.tar.gz!printf "=================================\nRemoved wget tidl_tools for AM62A \n================================\n!g'  /opt/ti/edgeai-tidl-tools/setup.sh
+#export TIDL_TOOLS_PATH=/opt/ti/edgeai-tidl-tools/tools/AM62A
+
+#sed -i 's!wget --quiet   https://software-dl.ti.com/jacinto7/esd/tidl-tools/$REL/TIDL_TOOLS/${SOC^^}/tidl_tools.tar.gz!printf "********** Removed wget tidl_tools for AM62A **********"!g'  /opt/ti/edgeai-tidl-tools/setup.sh
 
 
 cd /opt/ti/edgeai-tidl-tools
-source ./setup.sh
 
+printf "now run :-\nexport TIDL_TOOLS_PATH=/opt/ti/edgeai-tidl-tools/tools/AM62A; cd /opt/ti/edgeai-tidl-tools; source ./setup.sh\n\n"
+
+
+#source ./setup.sh
+#source ./setup.sh --skip_x86_python_install --skip_model_optimizer --skip_arm_gcc_download --skip_cpp_deps
 
 # fails on 
 # wget  https://software-dl.ti.com/jacinto7/esd/tidl-tools/11_00_06_00/TIDL_TOOLS/AM62A/tidl_tools.tar.gz
