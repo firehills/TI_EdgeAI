b109dddaa3002c62923fde597195f931224ebfd9
Philip Hill <hill.philip@gmail.com>
2025-09-19 13:22:51 +0100

    (chore) upgrade python from 3.10.16 to 3.10.18

--- install2.sh
+++ install2.sh
@@ -6,14 +6,14 @@ export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init - bash)"
 
-pyenv install 3.10.16
-pyenv virtualenv 3.10.16 py31016
+pyenv install 3.10.18
+pyenv virtualenv 3.10.18 py31018
 pyenv rehash
-pyenv activate py31016
+pyenv activate py31018
 pip3 install --no-input --upgrade pip==24.2 setuptools==73.0.0
 pip3 install --no-input cython wheel numpy==1.23.0
 #python -m pip install --upgrade pip
 
 
 
-printf "=============================================================\nNow run :- \npyenv activate py31016\n\n"
+printf "=============================================================\nNow run :- \npyenv activate py31018\n\n"
